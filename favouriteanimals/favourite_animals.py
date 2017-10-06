ea4755cc2d22b5bf1ca521774188aaa84c383a5f
AHolcsik <annamaria.holcsik@gmail.com>
2017-10-06 10:09:38 +0200

    feat(fav_animals): controller and functions added to app, can print from file and add new elements

--- favouriteanimals/favourite_animals.py
+++ favouriteanimals/favourite_animals.py
@@ -7,3 +7,38 @@
 # One animal should be stored only at once
 # Each animal should be written in separate lines
 # The program should only save animals, no need to print them
+
+import sys
+file_name = 'favourites.txt'
+
+def app_arg():
+    return len(sys.argv)
+
+def app_controller():
+    if app_arg() == 1:
+        print_favs()
+    elif app_arg() == 2:
+        add_animal()
+    else:
+        print('Unsuported argument')
+
+
+def print_favs():
+    try:
+        f = open(file_name, 'r')
+        for animal in f.readlines():
+            print(animal)
+            f.close()
+    except IOError:
+        print('Unable to read file: ', file_name)
+
+def add_animal():
+    try:
+        f = open(file_name, 'a')
+        f.write(sys.argv[1] + '\n')
+    except IOError:
+        print('Unable to read file: ', file_name)  
+
+
+
+app_controller()
