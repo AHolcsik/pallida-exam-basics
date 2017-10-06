c24bd21fbdff06c70c86ea658ce4b88123dc57a1
AHolcsik <annamaria.holcsik@gmail.com>
2017-10-06 10:48:46 +0200

    refractor(fav_animals): cleaned some code, no progress

--- favouriteanimals/favourite_animals.py
+++ favouriteanimals/favourite_animals.py
@@ -29,13 +29,30 @@ def print_animals():
         print('Unable to read file: ', file_name)
 
 def add_animal():
-    try:
-        f = open(file_name, 'a')
-        for element in sys.argv[1:]:
-            f.write(element + '\n')
-    except IOError:
-        print('Unable to read file: ', file_name)  
-
+    new_animals = sys.argv[1:]
+    f = open(file_name, 'a+')
+    for animal in new_animals:
+        if animal not in f.readlines():
+            try:
+                f.write(animal + '\n')
+            except IOError:
+                print('Unable to read file: ', file_name)
+    
 
 
 app_controller()
+
+
+
+
+# def add_animal():
+#     try:
+#         f = open(file_name, 'r')
+#         for animal in f.readlines:
+#             if animal != 
+#         try:
+#             f = open(file_name, 'a')
+#             for element in sys.argv[1:]:
+#                 f.write(element + '\n')
+#     except IOError:
+#         print('Unable to read file: ', file_name)  
